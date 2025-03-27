282d0cb4819cd643be254ec26fcb42f0cf76a5c9
martin setbon <setbon.martin@gmail.com>
2025-03-27 23:54:54 +0900

    hey

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
-/* eslint-disable no-undef */
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-workshop-ref/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'slides.html'),
-      },
-    },
-  },
-});
+
+  base: "/react-quiz/"
+})
